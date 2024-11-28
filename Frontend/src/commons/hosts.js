4de325ff1acc1bbf27323600692baec2d72e4495
VladLazar4 <lazar.vlad04@gmail.com>
2024-11-28 13:41:57 +0200

    final project

--- Frontend/src/commons/hosts.js
+++ Frontend/src/commons/hosts.js
@@ -1,11 +1,17 @@
 export const HOST_USER = {
+    // backend_api: 'http://reverse-proxy/userapplication',
     backend_api: 'http://userapplication.localhost',
 };
 
 export const HOST_DEVICE = {
     backend_api: 'http://deviceapplication.localhost',
+    // backend_api: 'http://reverse-proxy/deviceapplication',
 };
 
 export const HOST_MEASUREMENT = {
     backend_api: 'http://measurementapplication.localhost',
+    // backend_api: 'http://reverse-proxy/measurementapplication',
 };
+// export const HOST_USER = process.env.REACT_APP_USER_API_URL;
+// export const HOST_DEVICE = process.env.REACT_APP_DEVICE_API_URL;
+// export const HOST_MEASUREMENT = process.env.REACT_APP_MEASUREMENT_API_URL;
